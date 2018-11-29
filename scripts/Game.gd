f6a925152d6506db5bf6ef2990f2e050d53f6b99
Manuel Garcia <alexgarcia2k@gmail.com>
2018-11-28 17:56:40 -0800

    Creating tree.

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -1,8 +1,20 @@
 extends Node
 
 export(PackedScene) var trunk_scene
-func _ready():
 
+onready var firstTrunkPos = $FirstTrunkPosition
+
+var last_spawn_position
+
+func _ready():
+	last_spawn_position = firstTrunkPos.position
+	_spawn_first_trunks()
 	pass
 
+func _spawn_first_trunks():
+	for counter in range(5):
+		var newTrunk = trunk_scene.instance()
+		add_child(newTrunk)
+		newTrunk.position = last_spawn_position
+		last_spawn_position.y -= newTrunk.sprite_height
 

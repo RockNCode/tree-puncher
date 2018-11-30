c758a88f8e1fb4c4e2121c7fc37e84c3d62b2cb1
Manuel Garcia <alexgarcia2k@gmail.com>
2018-11-29 23:14:58 -0800

    spawning trunks.

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -5,6 +5,9 @@ export(PackedScene) var trunk_scene
 onready var firstTrunkPos = $FirstTrunkPosition
 
 var last_spawn_position
+var last_has_axe = false
+var last_axe_right = false
+
 var trunks = []
 
 func _ready():
@@ -21,6 +24,32 @@ func _spawn_first_trunks():
 		newTrunk.initialize_trunk(false,false)
 		trunks.append(newTrunk)
 		
+func _add_trunk(axe, axe_right):
+	var newTrunk = trunk_scene.instance()
+	add_child(newTrunk)
+	newTrunk.position = last_spawn_position
+	newTrunk.initialize_trunk(axe,axe_right)
+	trunks.append(newTrunk)
+
 func punch_tree(from_right):
+	if !last_has_axe:
+		if rand_range(0,100) > 50:
+			last_axe_right = rand_range(0,100) > 50
+			last_has_axe = true
+			#spawn trunk
+		else:
+			last_has_axe = false
+			#spawn trunk
+	else:
+		if rand_range(0,100) > 50 :
+			last_has_axe = true
+			#spawn trunk same position as last
+		else:
+			last_has_axe = false
+			#spawn trunk without axe
+	_add_trunk(last_has_axe,last_axe_right)
+
 	trunks[0].remove(from_right)
 	trunks.pop_front()
+	for trunk in trunks:
+		trunk.position.y += trunk.sprite_height

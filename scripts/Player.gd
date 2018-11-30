47c8f6db2ab1d5e14a4d75699b4f12c6e5690673
Manuel Garcia <alexgarcia2k@gmail.com>
2018-11-29 18:00:13 -0800

    moving the player.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,13 +1,22 @@
 extends Node2D
 
 onready var animation = $Animation
+var center
+var offset
 
 func _ready():
-
-	pass
-
+	center = get_viewport_rect().size.x / 2
+	offset = abs(position.x - center)
+	
 func _input(event):
 	if (event is InputEventMouseButton or 
 	event is InputEventScreenTouch ) and event.is_pressed():
 		animation.play("punch");
 		
+	# player clicked left of the screen
+		if event.position.x < center:
+			position.x = center - offset
+			scale.x = -abs(scale.x)
+		else:
+			position.x = center + offset	
+			scale.x = abs(scale.x)
